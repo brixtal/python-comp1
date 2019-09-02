3bc127ceb79f6d2d6366018c4144b852058647f0
André Brito <dentis99@gmail.com>
2019-09-01 21:26:11 -0300

    Criando exemplo de entrega da lista 1

---
+++ 2019.2/exemplo_entrega_lista_extra_1/110148819_Andre_Brito_ListaExtra1.py
@@ -0,0 +1,10 @@
+#Neste exemplo, a questão 1 pede a média de dois números e a questão 2 pede o valor da determinante de uma equação de 2º grau.
+
+#QUESTÃO 1
+def media(num1, num2):
+  return (num1+num2)/2
+  
+  
+#QUESTÃO 2
+def determinante(a, b, c):
+  return b**2 - 4 * a * c
